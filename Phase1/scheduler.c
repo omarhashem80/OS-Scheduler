543a5a5ed10263f79a7f8eae5b8a4c521beb35a0
omarhashem80 <114051743+omarhashem80@users.noreply.github.com>
2024-04-25 23:13:55 +0200

    Update scheduler.c

--- Phase1/scheduler.c
+++ Phase1/scheduler.c
@@ -1,89 +1,203 @@
+#include <math.h>
 #include "headers.h"
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-
+#include<string.h>
+struct Queue process_queue;
+struct Process * running_process;
+struct Process * terminated_process;
+struct msgbuff message;
+int total_cpu_time;
+int ideal_time=0;
+int total_non_ideal_time=0;
+float avg_WTA=0;
+float sum_WTA_squared=0;
+float avg_wating_time=0;
+float std_WTA=0;
+int number_of_processes=0;
+int q_key = 22;
+int q_id;
+int no_more_processes_to_recieve=0;
+FILE *outputFilePointer; // Declare a file pointer
+void process_terminated(int signum);
+void process_generator_terminated(int signum);
+void rr_start(int quantum);
+void write_scheduler_perf();
+void update_PCB(struct Process * p,int finished){
+    //waiting time =current time- arrival time-time taken in cpu uptil now
+    p->waiting_time=getClk()-p->arrival-(p->runtime-p->remaining_time);
+    fprintf(outputFilePointer, "At time\t%d\tprocess\t%d\t%s\tarrive\t%d\ttotal\t%d\tremain\t%d\twait\t%d",getClk(),p->id,p->state,p->arrival,p->runtime,p->remaining_time,p->waiting_time);
+    if(finished){
+        sum_WTA_squared+=p->WTA * p->WTA;
+        avg_wating_time+=running_process->waiting_time;
+        avg_WTA+=running_process->WTA;
+        number_of_processes++;
+        total_non_ideal_time+=p->runtime;
+        fprintf(outputFilePointer,"\tTA\t%d\tWTA\t%.2f\n",running_process->turnaround_time,running_process->WTA);
+    }else
+        fprintf(outputFilePointer,"\n");
+}
 int main(int argc, char *argv[]) {
-    printf("Hello from scheduler\n");
-    printf("Scheduler\n");
-    printf("algo: %s\n", argv[1]); 
-    printf("timeSlice: %s\n", argv[2]); 
+    signal(SIGUSR1, process_terminated);
+    signal(SIGUSR2, process_generator_terminated);
+    printf("FROM SCHDULER FILE :: Hello from scheduler\n");
+    printf("FROM SCHDULER FILE :: Scheduler\n");
+    printf("FROM SCHDULER FILE :: algo: %s\n", argv[1]); 
+    printf("FROM SCHDULER FILE :: timeSlice: %s\n", argv[2]); 
+    outputFilePointer = fopen("./outputs/scheduler.log", "w");
+    if (outputFilePointer == NULL) {
+        printf("Error opening the file.\n");
+        return 1; // Exit with an error code
+    }
+    q_id = msgget(q_key, 0666 | IPC_CREAT);
+    if (q_id == -1){
+        perror("Error in create up queue");
+        exit(-1);
+    }
+    initializeQueue(&process_queue);
     initClk();
-
-    // int q_key = 22;
-    // int quantum;
-    // int q_id = msgget(q_key, 0666 | IPC_CREAT);
-    // if (q_id == -1){
-    //     perror("Error in create up queue");
-    //     exit(-1);
-    // }
-    // struct Queue process_queue;
-    // initializeQueue(&process_queue);
-    // struct msgbuff message;
-    // //TODO implement the scheduler :)
-    // //upon termination release the clock resources.
-    // if(atoi(argv[1])==3){ //round robin
-    //     quantum=atoi(argv[2]);
-    //     int pick_new_process=quantum;
-    //     int next_time=getClk();
-    //     struct Process * running_process=NULL;
-    //     while (1)
-    //     {     
-    //         int rec_val= msgrcv(q_id, &message, sizeof(message.process),5, IPC_NOWAIT);
-    //         printf("shceduler recieves a new process with id:%d",message.process->id);
-    //         if(rec_val!=-1){//new process arrives
-    //             //fork new process
-    //             int pid=fork();
-    //             // Convert the integer to a string
-    //             char runtime[20]; 
-    //             sprintf(runtime, "%d", message.process->runtime);
-    //             if(pid==0)
-    //                 execl("process.out",runtime,message.process->id,NULL);
-                
-    //             kill(pid,SIGSTOP);
-    //             message.process->actual_id=pid;
-    //             //message.process->excution_time=0;
-    //             message.process->remaining_time=message.process->runtime;
-    //             enqueue(&process_queue,message.process);
-    //             //go into while loop each time send the new remaining time to this process
-
-    //         }else{
-    //             printf("NO process arrive this time slice");
-    //         }
-
-    //         if((pick_new_process==quantum)&& !isEmpty(&process_queue)){
-    //             //pick new process
-    //             if(running_process!=NULL){
-    //                 kill(running_process->actual_id,SIGSTOP);
-    //                 running_process->state=WAITING;
-    //                 if(running_process->remaining_time==0){
-    //                     running_process->state=TERMINATED;
-    //                     running_process->turnaround_time=getClk()-running_process->arrival;
-    //                     //Waiting Time = Turnaround Time - running time
-    //                     running_process->waiting_time=running_process->turnaround_time-running_process->runtime;
-    //                 }
-    //             }
-    //             running_process= dequeue(&process_queue);
-    //             pick_new_process=0;
-    //             running_process->state=RUNNING;
-    //             kill(running_process->actual_id,SIGCONT);
-
-    //         }
-    //         if((next_time+1)==getClk()){
-    //             //new time unit
-    //             //you should update process 
-    //              next_time++;
-    //              pick_new_process++;
-    //              running_process->remaining_time--;
-    //              running_process->state=RUNNING;
-    //            }
-    //         else
-    //             continue;
-
-    //     }
-    // }
-    sleep(5);// remove it
-    destroyClk(true);
+    if(atoi(argv[1])==3){ //round robin
+        rr_start(atoi(argv[2]));
+    }
+    fclose(outputFilePointer);
+    sleep(5);
+    destroyClk(false);
+    printf("\n-------------------------------------------------------\n");
+    printf("\n-------------------------------------------------------\n");
+    printf("\n\t\t\tscheduer finished\n");
+    printf("\n-------------------------------------------------------\n");
+    printf("\n-------------------------------------------------------\n");
     return 0;
+}
+void process_terminated(int signum){
+   if(terminated_process!=NULL){
+    printf("\n Process %d WAS TERMINATED at %d ",terminated_process->id,terminated_process->turnaround_time);
+    free(terminated_process);  
+    terminated_process=NULL;
+   }else 
+        printf("TERMINATION NOT DONT YET\n");
+}
+void process_generator_terminated(int signum){
+    no_more_processes_to_recieve=1;
+}
+void rr_start(int quantum){
+        terminated_process=NULL;
+        int pick_new_process=quantum;
+        int next_time=getClk();
+        running_process=NULL;
+        while (running_process!=NULL||!no_more_processes_to_recieve||!isEmpty(&process_queue))
+        {  
+            if((next_time+1)==getClk()){
+                printf("next time:%d clk:%d\n",next_time,getClk());
+                //new time unit
+                //you should update process 
+                 next_time++;
+                 if(running_process!=NULL){
+                    running_process->remaining_time--;
+                    printf("remaing time of process:%d is %d\n",running_process->id,running_process->remaining_time);
+                    if(running_process->remaining_time==0){
+                        pick_new_process=quantum;
+                    }else
+                    pick_new_process++;
+                 }else{
+                    ideal_time++;
+                    printf("/////////*************////////////cpu is ideal now \n");
+                 }
+            }   
+            int rec_val= msgrcv(q_id, &message, sizeof(message.process),0, IPC_NOWAIT);
+            //printf("FROM SCHDULER FILE :: HI:%d \n",message.process->id);
+            //printf("FROM SCHDULER FILE :: shceduler recieved val%d \n",rec_val);
+            int f=0;
+            if(rec_val!=-1){//new process arrives
+                printf("FROM SCHDULER FILE :: new process arrived at:%d\n",message.process.arrival);                
+                // Convert the integer to a string
+                char runtime[20]; 
+                sprintf(runtime, "%d", message.process.runtime);
+                char id [20]; 
+                sprintf(id, "%d", message.process.id);
+                int pid=fork();
+                if(pid==0){
+                   // printf("FROM SCHDULER FILE :: process id :%d\n",getpid());
+                   // execl("./process.out",runtime,message.process.id,NULL);
+                   char* processAgruments[]={"process.out", runtime,id, NULL};
+                    execv(realpath("process.out", NULL),processAgruments);
+                }
+                struct Process * p=malloc(sizeof(struct Process));
+                p->actual_id=pid;
+                p->runtime=message.process.runtime;
+                p->arrival=message.process.arrival;
+                p->id=message.process.id;
+                p->remaining_time=message.process.runtime;
+                strcpy(p->state,"stopped\0");
+                printf("FROM SCHDULER FILE :: schulder id :%d\n",getpid());
+                p->priority=message.process.priority;
+                enqueue(&process_queue,p);
+                kill(pid,SIGTSTP);
+            }
+
+            if((pick_new_process==quantum)){
+                if(running_process!=NULL){
+                    int f=0;
+                    printf("**********************************");
+                    printf("singal should be passed to %d",running_process->actual_id);
+                    printf("**********************************\n");
+                    printf("remaining time should be zero:%d\n",running_process->remaining_time);
+                    if(running_process->remaining_time==0){
+                        //todo: wait the process for exit code
+                        f=1;
+                        printf("/////////////////////////////////\n");
+                        printf("Process should terminats if side\n");
+                        printf("/////////////////////////////////\n");
+                        strcpy(running_process->state,"finish");
+                        running_process->turnaround_time=getClk()-running_process->arrival;
+                        //Waiting Time = Turnaround Time - running time
+                        running_process->WTA=running_process->turnaround_time*1.0/running_process->runtime;
+                        running_process->waiting_time=running_process->turnaround_time-running_process->runtime;
+                        terminated_process=running_process;                        
+                    }else{
+                        strcpy(running_process->state,"stopped");
+                        kill(running_process->actual_id,SIGTSTP);
+                        printf(" proces ID:%d entered the queue again with remainig time %d queue size :%d\n",running_process->id,running_process->remaining_time,process_queue.size);
+                
+                        enqueue(&process_queue,running_process);
+                    }
+                    
+                    update_PCB(running_process,f);                            
+                }
+                running_process=NULL;
+                //pick new process
+                if(!isEmpty(&process_queue)){
+                    printf("**********************************");
+                    printf("number of processes in the queue:%d",process_queue.size);
+                    printf("**********************************\n");
+                    running_process= dequeue(&process_queue);
+                    pick_new_process=0;
+                    strcpy(running_process->state,"resumed");
+                    if(running_process->remaining_time==running_process->runtime){
+                        strcpy(running_process->state,"started");
+                    }
+                    update_PCB(running_process,0);
+                    kill(running_process->actual_id,SIGCONT);
+                }
+            }
+            
+        }
+    total_cpu_time=getClk();
+    write_scheduler_perf();
+}
+void write_scheduler_perf(){
+    FILE *perfFilePointer; 
+    perfFilePointer = fopen("./outputs/scheduler.perf", "w");
+    //fprintf(perfFilePointer,"total cpu time\t%d\tideal time\t%d\n",total_cpu_time,ideal_time);
+    fprintf(perfFilePointer,"CPU Utilization\t%.2f\n",(total_cpu_time-ideal_time)*100.0/total_cpu_time);
+    //fprintf(perfFilePointer,"CPU Utilization\t%.2f\n",total_non_ideal_time*1.0/total_cpu_time);
+    avg_wating_time/=number_of_processes;
+    avg_WTA/=number_of_processes;
+    std_WTA=(sum_WTA_squared/number_of_processes) - (avg_WTA*avg_WTA);
+    std_WTA=sqrt(std_WTA);
+    fprintf(perfFilePointer,"Avg WTA\t%.2f\n",avg_WTA);
+    fprintf(perfFilePointer,"Avg Waiting\t%.2f\n",avg_wating_time);
+    fprintf(perfFilePointer,"std WTA\t%.2f\n",std_WTA);
 
 }
