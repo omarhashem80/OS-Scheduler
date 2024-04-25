477c5b9c0f26087f49558968393101b34838e72b
omar <omarsayed4736@gmail.com>
2024-04-25 22:29:01 +0200

    phase2

---
+++ Phase2/headers.h
@@ -0,0 +1,175 @@
+#include <stdio.h>      //if you don't use scanf/printf change this include
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/file.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+#include <sys/msg.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define true 1
+#define false 0
+
+#define SHKEY 300
+
+//***********************************************************************************//
+// Define a structure for the process
+struct Process {
+    int id, arrival, runtime, priority,actual_id,remaining_time,waiting_time,turnaround_time;
+    enum {
+        RUNNING,
+        TERMINATED,
+        WAITING
+    } state;
+};
+
+// Define a structure for the node in the queue
+struct Node {
+    struct Process *data;
+    struct Node *next;
+};
+
+// Define a structure for the queue
+struct Queue {
+    struct Node *front;
+    struct Node *rear;
+    int size;
+};
+
+// Function to initialize the queue
+void initializeQueue(struct Queue *queue) {
+    queue->front = NULL;
+    queue->rear = NULL;
+    queue->size = 0;
+}
+
+// Function to enqueue a process
+void enqueue(struct Queue *queue, struct Process *p) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    if (!newNode) {
+        perror("Memory allocation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    newNode->data = p;
+    newNode->next = NULL;
+
+    if (!queue->front) {
+        queue->front = queue->rear = newNode;
+    } else {
+        queue->rear->next = newNode;
+        queue->rear = newNode;
+    }
+    queue->size++;
+}
+
+// Function to dequeue a process
+struct Process* dequeue(struct Queue *queue) {
+    if (queue->front == NULL) return NULL;
+
+    struct Node *temp = queue->front;
+    struct Process *p = temp->data;
+    queue->front = queue->front->next;
+
+    if (queue->front == NULL) queue->rear = NULL;
+
+    free(temp);
+    queue->size--;
+    return p;
+}
+
+struct Process* peek(struct Queue *queue) {
+    if (queue->front == NULL) return NULL;
+    struct Node *temp = queue->front;
+    struct Process *p = temp->data;
+    return p;
+}
+
+// Function to check if the queue is empty
+int isEmpty(struct Queue *queue) {
+    return queue->size == 0;
+}
+
+// Function to destroy the queue and deallocate memory
+void destroyQueue(struct Queue *queue) {
+    // Traverse the queue and deallocate memory for each node
+    struct Node *current = queue->front;
+    while (current != NULL) {
+        struct Node *temp = current;
+        current = current->next;
+        
+        // Check if process data exists before deallocating
+        if (temp->data != NULL) {
+            free(temp->data); // Deallocate memory for the process data
+        }
+        
+        free(temp);       // Deallocate memory for the node
+    }
+
+    // Reset queue attributes
+    queue->front = NULL;
+    queue->rear = NULL;
+    queue->size = 0;
+}
+
+//***********************************************************************************//
+
+struct msgbuff
+{
+	long mtype;
+	struct Process* process;
+};
+
+///==============================
+//don't mess with this variable//
+int * shmaddr;                 //
+//===============================
+
+
+
+int getClk()
+{
+    return *shmaddr;
+}
+
+
+/*
+ * All process call this function at the beginning to establish communication between them and the clock module.
+ * Again, remember that the clock is only emulation!
+*/
+void initClk()
+{
+    int shmid = shmget(SHKEY, 4, 0444);
+    while ((int)shmid == -1)
+    {
+        //Make sure that the clock exists
+        printf("Wait! The clock not initialized yet!\n");
+        sleep(1);
+        shmid = shmget(SHKEY, 4, 0444);
+    }
+    shmaddr = (int *) shmat(shmid, (void *)0, 0);
+}
+
+
+/*
+ * All process call this function at the end to release the communication
+ * resources between them and the clock module.
+ * Again, Remember that the clock is only emulation!
+ * Input: terminateAll: a flag to indicate whether that this is the end of simulation.
+ *                      It terminates the whole system and releases resources.
+*/
+
+void destroyClk(bool terminateAll)
+{
+    shmdt(shmaddr);
+    if (terminateAll)
+    {
+        killpg(getpgrp(), SIGINT);
+    }
+}
---
+++ Phase2/process_generator.c
@@ -0,0 +1,111 @@
+#include "headers.h"
+
+int q_id;
+struct Queue processQueue;
+
+void clearResources(int signum)
+{
+    //TODO Clears all resources in case of interruption
+    msgctl(q_id, IPC_RMID, (struct msqid_ds *)0);
+    kill(0, SIGKILL);
+    destroyClk(true);
+    destroyQueue(&processQueue);
+    exit(0);
+}
+
+// Function to read the input file and populate the queue
+void readInputFile(const char *filename, struct Queue *queue) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[100];
+    // Read and discard the first line (header)
+    fgets(line, sizeof(line), file);
+
+    // Read process information from the file and enqueue them into the queue
+    while (fgets(line, sizeof(line), file) != NULL) {
+        struct Process *p = malloc(sizeof(struct Process));
+        if (p == NULL) {
+            perror("Memory allocation failed");
+            exit(EXIT_FAILURE);
+        }
+        sscanf(line, "%d %d %d %d", &p->id, &p->arrival, &p->runtime, &p->priority);
+        enqueue(queue, p);
+    }
+
+    // Close the file
+    fclose(file);
+}
+int main(int argc, char * argv[])
+{
+    signal(SIGINT, clearResources);
+    int algorithmNO,timeSlice = -1;   
+    char * path = argv[2];
+    initializeQueue(&processQueue);
+    readInputFile(path,&processQueue);
+    algorithmNO = atoi(argv[1]);
+    timeSlice = atoi(argv[3]);
+    for(int i=1; i< argc; i++)
+        printf("%s\n",argv[i]);
+
+    // 3. Initiate and create the scheduler and clock processes.
+
+    int pid = fork();
+    if (pid == -1) {
+        perror("Fork failed");
+        exit(EXIT_FAILURE);
+    } else if (pid == 0) {
+        //printf("child1 to scheduler\n");
+        char algorithmArg[16], timeSliceArg[16]; // Assuming algorithmNO and timeSlice won't exceed 15 digits
+        sprintf(algorithmArg, "%d", algorithmNO);
+        sprintf(timeSliceArg, "%d", timeSlice);
+        char *schedulerARGS[] = {"scheduler.out", algorithmArg, timeSliceArg, NULL}; 
+        execv(realpath("scheduler.out", NULL),schedulerARGS);
+    } else{
+        pid = fork();
+        if(pid==-1){
+            perror("Fork failed");
+            exit(EXIT_FAILURE);
+        }else if (pid==0){
+            char * clockARGS [] = {"clk.out", NULL};
+            //printf("child2 to clk\n");
+            execv(realpath("clk.out", NULL),clockARGS);
+        }
+    }
+    // 4. Use this function after creating the clock process to initialize clock
+    initClk();
+    // To get time use this
+    int x = getClk();
+    printf("current time is %d\n", x);
+    // TODO Generation Main Loop
+    // 5. Create a data structure for processes and provide it with its parameters.
+    // 6. Send the information to the scheduler at the appropriate time.
+    int q_key = 22;
+    q_id = msgget(q_key, 0666 | IPC_CREAT);
+     if (q_id == -1){
+        perror("Error in create up queue");
+        exit(-1);
+    }
+    while(!isEmpty(&processQueue)){
+        struct Process *p = peek(&processQueue);
+        if(p->arrival== getClk()){
+            struct msgbuff message;
+            message.process = p;
+            message.mtype = 5;
+            int send_val = msgsnd(q_id,  &message, sizeof(message.process), !IPC_NOWAIT);
+            if (send_val == -1)
+                perror("Errror in send");
+
+            dequeue(&processQueue);
+        }
+        
+    }
+    // 7. Clear clock resources
+    destroyClk(true);
+    // 8. Clear queue resources
+    destroyQueue(&processQueue);
+    return 0;
+}
